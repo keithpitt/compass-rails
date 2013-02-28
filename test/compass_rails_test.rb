ff7533c5cedc83a4e6f7ca37428f3691bf50966c
Keith Pitt <me@keithpitt.com>
2013-02-28 18:55:31 +1100

    Removing tests for old versions of Rails.

--- test/compass_rails_test.rb
+++ test/compass_rails_test.rb
@@ -1,17 +1,15 @@
 require 'test_helper'
-class Rails31Test < Test::Unit::TestCase
+class CompassRailsTest < Test::Unit::TestCase
   include CompassRails::Test::RailsHelpers
 
-  RAILS_VERSION = RAILS_3_1
-
   def test_rails_app_created
-    within_rails_app('test_railtie', RAILS_VERSION) do |project|
+    within_rails_app('test_railtie') do |project|
       assert project.boots?
     end
   end
 
   def test_installs_compass
-    within_rails_app('test_railtie', RAILS_VERSION) do |project|
+    within_rails_app('test_railtie') do |project|
       project.run_compass('init')
       assert project.has_config?
       assert project.has_screen_file?
@@ -20,7 +18,7 @@ class Rails31Test < Test::Unit::TestCase
   end
 
   def test_compass_compile
-    within_rails_app('test_railtie', RAILS_VERSION) do |project|
+    within_rails_app('test_railtie') do |project|
       project.run_compass('init')
       project.run_compass('compile')
       assert project.directory.join('public/assets/screen.css').exist?
@@ -28,20 +26,18 @@ class Rails31Test < Test::Unit::TestCase
   end
 
   def test_install_blueprint
-    within_rails_app('test_railtie', RAILS_VERSION) do |project|
+    within_rails_app('test_railtie') do |project|
       project.run_compass('init')
       project.run_compass('install blueprint --force')
       assert project.directory.join('app/assets/stylesheets/partials').directory?
     end
   end
 
-
   def test_compass_preferred_syntax
-    within_rails_app('test_railtie', RAILS_VERSION) do |project|
+    within_rails_app('test_railtie') do |project|
       project.set_rails('sass.preferred_syntax', :sass)
       project.run_compass('init')
       assert project.directory.join('app/assets/stylesheets/screen.css.sass').exist?
-    end    
+    end
   end
-
 end
