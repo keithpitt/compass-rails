c47a5cb4c94cfefb8d4e5263093a3b182e98cea8
Keith Pitt <me@keithpitt.com>
2013-02-28 19:12:56 +1100

    Including a rails 4 fixture project.

--- test/compass_rails_test.rb
+++ test/compass_rails_test.rb
@@ -1,13 +1,13 @@
 require 'test_helper'
 class CompassRailsTest < Compass::Rails::TestCase
   def test_rails_app_created
-    within_rails_app('test_railtie') do |project|
+    within_rails_app('compass_rails_project') do |project|
       assert project.boots?
     end
   end
 
   def test_installs_compass
-    within_rails_app('test_railtie') do |project|
+    within_rails_app('compass_rails_project') do |project|
       project.run_compass('init')
       assert project.has_config?
       assert project.has_screen_file?
@@ -16,7 +16,7 @@ class CompassRailsTest < Compass::Rails::TestCase
   end
 
   def test_compass_compile
-    within_rails_app('test_railtie') do |project|
+    within_rails_app('compass_rails_project') do |project|
       project.run_compass('init')
       project.run_compass('compile')
       assert project.directory.join('public/assets/screen.css').exist?
@@ -24,7 +24,7 @@ class CompassRailsTest < Compass::Rails::TestCase
   end
 
   def test_install_blueprint
-    within_rails_app('test_railtie') do |project|
+    within_rails_app('compass_rails_project') do |project|
       project.run_compass('init')
       project.run_compass('install blueprint --force')
       assert project.directory.join('app/assets/stylesheets/partials').directory?
@@ -32,7 +32,7 @@ class CompassRailsTest < Compass::Rails::TestCase
   end
 
   def test_compass_preferred_syntax
-    within_rails_app('test_railtie') do |project|
+    within_rails_app('compass_rails_project') do |project|
       project.set_rails('sass.preferred_syntax', :sass)
       project.run_compass('init')
       assert project.directory.join('app/assets/stylesheets/screen.css.sass').exist?
---
+++ test/fixtures/compass_rails_project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CompassRailsProject::Application.config.secret_key_base = 'cd3705b5299581ae4b7e79937ba7d75f201841580fa74860241bf59fe1a273fa64bc6039173b49558d887605c490299c83d9d7fc073aa90f07d9f43d75a3ea8f'
